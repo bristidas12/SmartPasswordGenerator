55ad673bafa6888f34c552c1f944391106c30374
bristidas12 <bristidas.ab@gmail.com>
2026-05-28 19:07:34 +0530

    Update Main.java

--- src/passwordgenerator/Main.java
+++ src/passwordgenerator/Main.java
@@ -1,57 +1,31 @@
 package project;
-
 import java.util.Scanner;
 import java.util.Random;
-
 public class SmartPasswordGenerator {
-
-    // Generate Password Method
+  // Generate Password Method
     public static String generatePassword(String name, String pan, String dob) {
-
-        Random random = new Random();
-
+     Random random = new Random();
         String special = "@#$&!";
-
         char sp = special.charAt(random.nextInt(special.length()));
-
         int num = random.nextInt(100);
-
-        String password =
-                name.substring(0, 2).toUpperCase() +
-                pan.substring(0, 4) +
-                dob.substring(0, 2) +
-                sp +
-                num;
-
-        return password;
+        String password = name.substring(0, 2).toUpperCase() + pan.substring(0, 4) +  dob.substring(0, 2) + sp + num;
+                        return password;
     }
-
     // Password Strength Method
     public static void checkStrength(String password) {
-
         if (password.length() >= 12) {
-
             System.out.println("Password Strength: STRONG");
-
         } else if (password.length() >= 8) {
-
             System.out.println("Password Strength: MEDIUM");
-
         } else {
-
             System.out.println("Password Strength: WEAK");
         }
     }
-
     // Main Method
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         char choice;
-
         do {
-
             // Accept User Details
             System.out.print("Enter Your Name: ");
             String name = sc.nextLine();
@@ -63,9 +37,8 @@ public class SmartPasswordGenerator {
             String dob = sc.nextLine();
 
             // Generate Password
-            String password =
-                    generatePassword(name, pan, dob);
-
+            String password = generatePassword(name, pan, dob);
+            
             // Display Password
             System.out.println("\nGenerated Password: " + password);
 
@@ -85,5 +58,3 @@ public class SmartPasswordGenerator {
         sc.close();
     }
 }
-
-
